2a8627e076844dd116f88607478cbcda43300be8
Alex Ott <alex.ott@datastax.com>
2018-12-07 12:44:55 +0100

    experimenting with Join & Java

---
+++ src/main/java/com/datastax/alexott/demos/spark/JoinTests.java
@@ -0,0 +1,56 @@
+package com.datastax.alexott.demos.spark;
+
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.RowFactory;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.StructField;
+import org.apache.spark.sql.types.StructType;
+import org.spark_project.guava.collect.ImmutableMap;
+import scala.Tuple1;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+// create table if not exists test.jtest (id int primary key, v text);
+
+public class JoinTests {
+    public static void main(String[] args) {
+        SparkSession spark = SparkSession
+                .builder()
+                .appName("CassandraSparkWithJoin")
+//                .config("spark.cassandra.connection.host", "192.168.0.10")
+                .getOrCreate();
+
+//        Dataset<Row> df = spark.sql("select * from test.jtest");
+//        df.show();
+        JavaRDD<Row> toJoinRDD = spark
+                .range(1, 1000)
+                .javaRDD()
+                .map((Function<Long, Row>) x -> {
+                    return RowFactory.create(x.intValue());
+                });
+
+        List<StructField> fields = new ArrayList<>();
+        fields.add(DataTypes.createStructField("id", DataTypes.IntegerType, true));
+        StructType schema = DataTypes.createStructType(fields);
+        Dataset<Row> toJoin = spark.createDataFrame(toJoinRDD, schema);
+        toJoin.printSchema();
+//        toJoin.show();
+
+        Dataset<Row> dataset = spark.read()
+                .format("org.apache.spark.sql.cassandra")
+                .options(ImmutableMap.of("table", "jtest", "keyspace", "test"))
+                .load();
+
+        Dataset<Row> joined = toJoin.join(dataset,
+                toJoin.col("id").equalTo(dataset.col("id")));
+        joined.printSchema();
+        joined.explain();
+        joined.show(10);
+    }
+}
--- src/main/scala/com/datastax/alexott/graphframes/DGFSubGraph.scala
+++ src/main/scala/com/datastax/alexott/graphframes/DGFSubGraph.scala
@@ -1,9 +1,16 @@
 package com.datastax.alexott.graphframes
 
-import java.util.Properties
+import org.apache.spark.SparkContext
+import org.apache.spark.sql.SparkSession
+//import com.datastax.bdp.graph.spark.graphframe._
 
 object DGFSubGraph {
-  def main(args: Array[String]): Unit = {
+    def main(args: Array[String]): Unit = {
+      val sc = new SparkContext()
+      val spark = SparkSession.builder().config(sc.getConf).getOrCreate()
+      import spark.implicits._
+
+//      val graphBuilder = spark.dseGraph("GRAPH_NAME")
 
 
   }
